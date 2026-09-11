29cfcc5049ce6a4ef6c338235c8a7aba519455d6
Nickoll <nickollandrearomeroguisado@gmail.com>
2026-09-11 18:47:27 -0500

    Se agrego el nombre y la defensa

---
+++ view/VistaPersonaje.php
@@ -0,0 +1,19 @@
+<?php
+
+class VistaPersonaje {
+    /** @param Personaje $personaje */
+
+    public function mostrarPersonaje(Personaje $personaje) {
+        echo $personaje -> atacar();
+    }
+
+    public function mostrarSeparador() {
+        echo str_repeat('-', 60);
+    }
+
+    /** @param string $titulo */
+    public function mostrarTitulo($titulo) {
+        echo "=== {$titulo} ===";
+        echo "<br>";
+    }
+}
